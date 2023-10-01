5f1244178be22d566975eb8ee48cd2de77ab246e
Nijin Joy <nijinjoy1999@gmail.com>
2023-10-01 18:05:06 +0530

    project creation and icon add

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hell</Text>
+    </View>
+  );
+}
+
+export default App;
